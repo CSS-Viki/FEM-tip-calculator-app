0e5ae45bb00f032a1b555254e6cbdba07a03c041
Viki Designs <vikdemo2023@gmail.com>
2023-12-07 16:51:53 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Frontend Mentor - Tip calculator app
+
+![Design preview for the Tip calculator app coding challenge](./design/desktop-preview.jpg)
+
+## Welcome! 👋
+
+
+[Frontend Mentor](https://www.frontendmentor.io) challenges help you improve your coding skills by building realistic projects.
+
+**To do this challenge, you need a basic understanding of HTML, CSS and JavaScript.**
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+
+**Have fun building!** 🚀
---
+++ script.js
@@ -0,0 +1,70 @@
+const tipValuesContainer = document.getElementById("tip-values");
+const custom = document.querySelector(".tip-custom");
+const tipAmount = document.getElementById("tip-amount");
+const billTotal = document.getElementById("total-amount");
+const totalPeople = document.getElementById("total-people");
+const totalAmountPp = document.getElementById("total-per-person");
+const reset = document.getElementById("reset-btn");
+const error = document.getElementById("error");
+
+custom.addEventListener("click", (e) => {
+  console.log(e.target.value);
+});
+
+let selectedElement = null;
+
+function tipCalculator() {
+  tipValuesContainer.addEventListener("click", (e) => {
+    if (e.target.classList.contains("tip-value")) {
+      const tips = parseInt(e.target.value);
+      tipAmount.textContent = `$${tips / totalPeople.value}`;
+
+      const clickedElement = e.target;
+
+      // Reset the background color of the previously selected element
+      if (selectedElement) {
+        selectedElement.style.backgroundColor = "hsl(183, 100%, 15%)";
+      }
+
+      // Set the background color of the clicked element
+      clickedElement.style.backgroundColor = "hsl(172, 67%, 45%)";
+
+      selectedElement = clickedElement; //tracking the selected element
+
+      if (billTotal.value === "" && totalPeople.value === "") {
+        totalAmountPp.textContent = `$${0.0}`;
+      } else {
+      const totalPerPerson = Math.round(billTotal.value / totalPeople.value);
+        totalAmountPp.innerHTML = `$${
+          totalPerPerson * (tips / 100) + totalPerPerson
+        }`;
+        reset.style.opacity = 1;
+      }
+      if (totalPeople.value === 0 || totalPeople.value === "") {
+        error.innerHTML = `Can't be 0`;
+        totalPeople.style.border = "1px";
+        totalPeople.style.borderStyle = "solid";
+        totalPeople.style.borderColor = "red";
+        totalAmountPp.textContent = `$${0}`;
+        tipAmount.textContent = `$${0}`;
+        reset.style.opacity = ".2";
+        // reset.disabled = true;
+      } else {
+        error.innerHTML = "";
+        totalPeople.style.border = "none";
+      }
+    }
+  });
+}
+
+//Reseting all values to default
+function resetValues() {
+  tipAmount.textContent = "$0";
+  totalAmountPp.textContent = "$0";
+  billTotal.value = "";
+  totalPeople.value = "";
+  reset.style.opacity = ".2";
+}
+
+reset.addEventListener("click", resetValues);
+tipCalculator();
